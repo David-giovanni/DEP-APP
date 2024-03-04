c997d43bd1fc97616feb239d0cf4c63d2a708335
David-giovanni <113347274+David-giovanni@users.noreply.github.com>
2024-03-04 14:43:41 +0100

    Change MyAds

--- Dep-App/Front/mon-app/src/Components/MyAds.js
+++ Dep-App/Front/mon-app/src/Components/MyAds.js
@@ -35,7 +35,7 @@ const MyAds = () => {
     <div className="bg-[#242424] min-h-screen">
       <Navbar />
       <div className="max-w-2xl mx-auto p-6 ">
-        <h1 className="text-3xl text-white font-bold mb-6">My Ads</h1>
+        <h1 className="text-3xl text-white font-bold mb-6">Mes Annonces</h1>
         {userAds.map((ad) => (
           <div key={ad._id} className="bg-white rounded-xl p-4 mb-4">
             <h2 className="text-2xl font-bold text-blue-500 mb-2">
